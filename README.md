26b9bbd8f9576a0fdb61aabdea8c69e925b71af5
Kostiantyn Smyrnov <kostysh@gmail.com>
2019-04-06 16:01:52 +0300

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # promievent
-A better version of PromiEvent that act seamlessly as native Promise but with events
+A better version of PromiEvent (inspired by [web3's](https://github.com/ethereum/web3.js) PromiEvent) that acts seamlessly as native Promise but with events (not like a deferred object).  
+
+## Usage
+```javascript
+
+new PromiEvent((resolve, reject, event) => {
+    setTimeout(() => resolve('Done'));
+    event.emit('action', { a: 1 });
+    event.emit('action', { a: 2 });
+    event.emit('action', { a: 3 });
+    event.emit('only', { o: 1 });
+    event.emit('only', { o: 2 });
+    event.emit('only', { o: 3 });
+})
+    .then(console.log)
+    .on('action', console.log)
+    .once('only', console.log)
+    .catch(console.error)
+    .finally(() => console.log('Finally!!!'));
+
+// Output:
+//> { a: 1 }
+//> { a: 1 }
+//> { a: 1 }
+//> { o: 1 }
+//> Done
+//> Finally!!!
+
+```
---
+++ src/PromiEvent.js
@@ -0,0 +1,58 @@
+/**
+ * @file PromiEvent.js
+ * @author Kostiantyn Smyrnov <kostysh@gmail.com>
+ * @date 2019
+ */
+import EventEmitter from 'eventemitter3';
+
+/**
+ * PromiEvent class that acts seamlessly as native Promise but with events
+ * @class PromiEvent 
+ */
+export default class PromiEvent {
+    /**
+     * @constructor
+     */
+    constructor(executor) {
+        this.eventEmitter = new EventEmitter();
+        this.promise = new Promise((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+
+        const proxy = new Proxy(this, {
+            get: (target, name) => {
+
+                if (this.eventEmitter[name] && 
+                    typeof this.eventEmitter[name] === 'function') {
+
+                    return function (...args) {
+                        this.eventEmitter[name].apply(this.eventEmitter, args);
+                        return proxy;
+                    }.bind(proxy);
+                }
+
+                if (this.promise[name] && 
+                    typeof this.promise[name] === 'function') {
+
+                    return function (...args) {
+                        this.promise[name].apply(this.promise, args);
+                        return proxy;
+                    }.bind(proxy);
+                }
+
+                return target[name];
+            }
+        });
+
+        setTimeout(() => {
+            try {
+                executor(proxy.resolve, proxy.reject, proxy.eventEmitter);
+            } catch (err) {
+                proxy.reject(err);
+            }
+        });
+
+        return proxy;
+    }
+}
